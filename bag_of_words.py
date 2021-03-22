81473affbfc83de09b78d1bda899cfa4284fc9b3
Tsukimi <1027532960@qq.com>
2021-03-22 17:45:13 +0800

    freq sort

--- bag_of_words.py
+++ bag_of_words.py
@@ -9,7 +9,7 @@ word_bags = {}
 
 
 def computeRepo(repo_name):
-    print(8*'=', "start to handle repo ", repo_name, 8*'=')
+    print(8 * '=', "start to handle repo ", repo_name, 8 * '=')
     repo = Repo(os.path.join(repo_dir, repo_name))
     commits = list(repo.iter_commits())
 
@@ -24,13 +24,13 @@ def computeRepo(repo_name):
                     file) > 5 and file[-5:] == '.java':
                 kot_jav_cnt += 1
         if xml_cnt >= 1 and kot_jav_cnt >= 1:
-            for seg in jieba.cut(commit.message, cut_all=False):
+            for seg in jieba.cut(commit.message.lower(), cut_all=False):
                 if len(seg) > 1 and seg != '\t' and seg != '\n':
                     if seg in word_bags:
                         word_bags[seg] += 1
                     else:
                         word_bags[seg] = 1
-    print(8*'=', "repo ", repo_name, "done", 8*'=')
+    print(8 * '=', "repo ", repo_name, "done", 8 * '=')
 
 
 def goThroughAll():
@@ -43,8 +43,14 @@ def goThroughAll():
             assert (os.path.exists(os.path.join(repo_dir, repo_name)))
             computeRepo(repo_name)
 
+    word_freq = []
+    for word, freq in word_bags.items():
+        word_freq.append((word, freq))
+
+    word_freq.sort(key=lambda x: x[1], reverse=True)
+
     with open("bag_of_words_res", 'w') as res_fd:
-        for word, freq in word_bags.items():
+        for (word, freq) in word_freq:
             res_fd.write("{} {}\n".format(word, freq))
 
 
