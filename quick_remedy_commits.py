40bc2db826cc4a1a6a16bdf93fc3bfaac4c329a0
tannpopo <yuailun@pku.edu.cn>
2021-04-15 16:22:28 +0800

    filter

--- quick_remedy_commits.py
+++ quick_remedy_commits.py
@@ -11,12 +11,17 @@ else:
     work_dir = "/home/repos"
     stats_dir = "/home/yuailun/quick_remedy_commits"
 
+QRCommits_cnt = 0
+
 
 def getQRCommits(repo_name):
+    global QRCommits_cnt
+    repo_cnt = 0
+
     commits_list = []
     shas = set()
     repo = Repo(os.path.join(work_dir, repo_name))
-    
+
     for b in repo.remote().fetch():
         if '/' not in b.name:
             continue
@@ -27,7 +32,7 @@ def getQRCommits(repo_name):
 
         for idx, commit in enumerate(commits):
             if str(commit) in shas:
-                break
+                continue
             else:
                 shas.add(str(commit))
             commit_collection = {}
@@ -43,14 +48,25 @@ def getQRCommits(repo_name):
                     kot_jav_cnt += 1
             if xml_cnt >= 1 and kot_jav_cnt >= 1:
                 if idx != 0:
+                    valid = False
+                    for f in list(commits[idx - 1].stats.files):
+                        if '.xml' in f or '.kt' in f or '.java' in f:
+                            valid = True
+                            break
+                    if not valid:
+                        continue
                     time_next_commit = commits[
                         idx - 1].committed_datetime - commit.committed_datetime
                     if time_next_commit.total_seconds() / 60 <= 30 and len(
-                            commit.parents) < 2:
+                            commit.parents) < 2 and len(
+                                commits[idx - 1].parents) < 2:
+                        QRCommits_cnt += 1
+                        repo_cnt += 1
                         commit_collection["commit_id"] = str(commit)
-                        commit_collection["commit_msg"] = str.strip(commit.message)
                         commit_collection["commit_time"] = str(
                             commit.committed_datetime)
+                        commit_collection["commit_msg"] = str.strip(
+                            commit.message)
                         commit_collection["remedy_id"] = str(commits[idx - 1])
                         commit_collection["remedy_time"] = str(
                             commits[idx - 1].committed_datetime)
@@ -73,9 +89,10 @@ def getQRCommits(repo_name):
             # commit_collection["parent_commit_num"] = len(commit.parents)
 
     repo_dump = json.dumps(commits_list, indent=2, ensure_ascii=False)
-    with open(os.path.join(stats_dir, repo_name), "w") as commit_fd:
+    with open(os.path.join(stats_dir, repo_name + "_" + str(repo_cnt)),
+              "w") as commit_fd:
         commit_fd.write(repo_dump)
-        print(repo_name + " done")
+        print(repo_name + " done with {} commits".format(repo_cnt))
 
 
 def allRepos():
@@ -94,3 +111,5 @@ if platform == "linux":
     allRepos()
 elif platform == "darwin":
     getQRCommits("TeamNewPipe-NewPipe")
+
+print("done. all {} commits".format(QRCommits_cnt))
