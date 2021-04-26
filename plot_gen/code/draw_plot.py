124a2e27fd63876859e143c0be100940cc6b0ad4
tannpopo <yuailun@pku.edu.cn>
2021-04-26 10:27:40 +0800

    boxplot format

--- plot_gen/code/draw_plot.py
+++ plot_gen/code/draw_plot.py
@@ -11,9 +11,9 @@ csv_dir = "../csv"
 plot_dir = "../plots"
 
 for metric in metric_type:
-    sns.set_theme(style="ticks", palette="pastel")
+    sns.set_theme(style="ticks", palette="pastel", font_scale=1.05, font="Arial")
     data = pd.read_csv(os.path.join(csv_dir, metric + ".csv"))
-    f = plt.figure(figsize=[4, 6])
+    f = plt.figure(figsize=[2.5, 6])
     ax = f.add_subplot(111)
     plot = sns.boxplot(x="commit type",
                        y=metric + " count",
@@ -22,10 +22,13 @@ for metric in metric_type:
                        fliersize=2,
                        palette=["m", "g"],
                        dodge=False,
+                       width=1,
                        ax=ax)
     plot.set_yscale("log")
-    plot.set_ylim(bottom=1)
-    plot.set_ylabel("Number of " + metric)
+    # plot.set_ylim(bottom=1)
+    plot.set_xlim(-0.8, 1.8)
+    # ax.set_aspect(0.5)
+    plot.set_ylabel("Number of " + metric, fontsize=16)
     plot.set_xlabel("")
     f.tight_layout()
     fig = plot.get_figure()
