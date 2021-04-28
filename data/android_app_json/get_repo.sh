0df2ec67821d8779ef601e20db8df4d13c3b9443
tannpopo <yuailun@pku.edu.cn>
2021-04-28 15:54:09 +0800

    bash to get repos

--- data/android_app_json/get_repo.sh
+++ data/android_app_json/get_repo.sh
@@ -3,7 +3,7 @@ int=1
 while (($int<=10))
 do
 	a="android-java-repo-app-p${int}"
-	url="https://api.github.com/search/repositories?q=app+in:readme+topic:android+language:java&sort=stars&order=desc&per_page=100&page=${int}"
+	url="https://api.github.com/search/repositories?q=topic:android+language:java&sort=stars&order=desc&per_page=100&page=${int}"
 	curl -H "Accept: application/vnd.github.mercy-preview+json" $url >> $a
  	let "int++"
  done
